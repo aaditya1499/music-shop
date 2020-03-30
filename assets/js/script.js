f0b0f358ff7cb26e9f4350cec4f4efc1e2af7961
Aaditya Udit Shrivastava <aadityashrivastava@outlook.com>
2020-03-30 18:58:55 +0530

    made homepage responsive and animations to nav-bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,16 @@ $('.nav-link').on('click', function() {
 	$(this).addClass('active-link');
 });
 
+$('.nav-link').hover(function () {
+		console.log("called");
+		$(".active").children(".underline").hide("slow");
+	}, function () {
+		$(".active").children(".underline").show("slow");
+	}
+);
+
+var x = screen.width; 
+
 var cw = $('.featured-product-card').width();
-$('.featured-product-card').css({'height':cw+'px'});
+if(x>768)
+	$('.featured-product-card').css({'height':cw+'px'});
